f16890268dca164877779e1dac2121fa915c2d9f
samu <samueless001@gmail.com>
2024-01-15 14:45:07 +0100

    Esercizio 1

--- Esercizio-1/src/Main.java
+++ Esercizio-1/src/Main.java
@@ -1,15 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private static final int[] arr = new int[5];
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Esercizio 1");
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try{
+            for (int i = 0; i < arr.length; i++) {
+                int n = random.nextInt(10);
+                arr[i] = n;
+                System.out.println("---------- ARRAY " + (i + 1) + " ----------");
+                System.out.println(arr[i]);
+            }
+            do {
+                System.out.println("Inserire valore da cambiare da 1 a 5, 0-Esci");
+                int counter = input.nextInt();
+                if (counter == 0){
+                    break;
+                }
+                if (counter >= 1 && counter <= 5){
+                    System.out.println("Inserire nuovo valore");
+                    int m = input.nextInt();
+                    arr[counter - 1] = m;
+                    for (int i = 0; i < arr.length; i++) {
+                        System.out.println("---------- NUOVO ARRAY " + (i + 1) +  " ----------");
+                        System.out.println(arr[i]);
+                    }
+                }else {
+                    System.out.println("Valore non valido");
+                }
+            }while (true);
+        } catch (ArrayIndexOutOfBoundsException err){
+            System.err.println(err.getMessage());
         }
+
+
     }
 }
